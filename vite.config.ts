f682c2bfce77bdcab89fdf98a98d1b04b2caf85c
Valeriia Voloshyna <valeriia.voloshyna.work@gmail.com>
2026-09-03 15:05:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
 
-import siteConfiguration from './.figma/make/site.json'
+import siteConfiguration from './make/site.json'
 
 // Vite config — https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
